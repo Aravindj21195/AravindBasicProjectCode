a05bdc5ee14fb1469ffd03c7e78989b96ceb8806
Dell <Dell@DESKTOP-5UBQ2UJ>
2022-09-18 13:26:27 +0530

    First commit

---
+++ src/BasicJavaproject/BasicIfelseLoop.java
@@ -0,0 +1,15 @@
+package BasicJavaproject;
+
+public class BasicIfelseLoop {
+
+	public static void main(String[] args) {
+		String Hungry = "yes";
+		
+if(Hungry == "yes")
+System.out.println("It is correct");
+else
+{
+System.out.println("It is wrong");	
+}
+}
+}
